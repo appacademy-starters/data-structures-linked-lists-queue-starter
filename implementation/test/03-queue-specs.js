85ae0500fd0a121e78d5526f1633374d28022c72
Soon-Mi Sugihara <ssoonmi@gmail.com>
2021-02-17 17:44:52 -0500

    Modify starter to reflect instructions

--- implementation/test/03-queue-specs.js
+++ implementation/test/03-queue-specs.js
@@ -1,4 +1,4 @@
-const { Node, Queue } = require('../lib/queue.js');
+const { SinglyLinkedNode, Queue } = require('../03-queue.js');
 const { expect } = require('chai');
 
 describe('Queue', () => {
@@ -9,17 +9,17 @@ describe('Queue', () => {
     queue = new Queue();
   });
 
-  describe('Node Constructor', () => {
+  describe('SinglyLinkedNode Constructor', () => {
     it('Should exist', () => {
-      expect(Node).to.exist;
+      expect(SinglyLinkedNode).to.exist;
     });
 
     it('Should be a function (ES6 classes are "special functions")', () => {
-      expect(Node).to.be.a('function');
+      expect(SinglyLinkedNode).to.be.a('function');
     });
 
     it('Should have "value" and "next" properties', () => {
-      node = new Node('A');
+      node = new SinglyLinkedNode('A');
       expect(node).to.have.property('value');
       expect(node).to.have.property('next');
     });
@@ -34,10 +34,9 @@ describe('Queue', () => {
       expect(Queue).to.be.a('function');
     });
 
-    it('Should have front, back, and length properties', () => {
-      expect(queue).to.have.property('front');
-      expect(queue).to.have.property('back');
-      expect(queue).to.have.property('length');
+    it('Should have head, tail and length properties', () => {
+      expect(queue).to.have.property('head');
+      expect(queue).to.have.property('tail');
     });
 
     it('Should not implement an Array to store values', () => {
@@ -48,10 +47,9 @@ describe('Queue', () => {
   });
 
   describe('Queue Methods', () => {
-    it('Should have methods named "enqueue", "dequeue", and "size"', () => {
+    it('Should have methods named "enqueue" and "dequeue"', () => {
       expect(queue.enqueue).to.be.a('function');
       expect(queue.dequeue).to.be.a('function');
-      expect(queue.size).to.be.a('function');
     });
 
     describe('enqueue', () => {
@@ -61,31 +59,31 @@ describe('Queue', () => {
 
       it('Should create a new node with the value provided as input', () => {
         queue.enqueue('A');
-        expect(queue.front.value).to.equal('A');
+        expect(queue.head.value).to.equal('A');
       });
 
-      it('Should reassign both the front and back pointers when a new node is added to the back of an empty queue', () => {
+      it('Should reassign both the head and tail pointers when a new node is added to the tail of an empty queue', () => {
         queue.enqueue('A');
-        expect(queue.front.value).to.equal('A');
-        expect(queue.back.value).to.equal('A');
+        expect(queue.head.value).to.equal('A');
+        expect(queue.tail.value).to.equal('A');
       });
 
-      it('Should reassign only the back pointer when new nodes are added to the back of the queue of size one or greater', () => {
-        queue.enqueue('A');
-        queue.enqueue('B');
-        expect(queue.front.value).to.equal('A');
-        expect(queue.back.value).to.equal('B');
-        queue.enqueue('C');
-        expect(queue.front.value).to.equal('A');
-        expect(queue.back.value).to.equal('C');
-      });
-
-      it('Should point the next property of the front node to the node that follows it for queues of size one or greater', () => {
+      it('Should reassign only the tail pointer when new nodes are added to the tail of the queue of size one or greater', () => {
         queue.enqueue('A');
         queue.enqueue('B');
-        expect(queue.front.next.value).to.eql('B');
+        expect(queue.head.value).to.equal('A');
+        expect(queue.tail.value).to.equal('B');
         queue.enqueue('C');
-        expect(queue.front.next.value).to.equal('B');
+        expect(queue.head.value).to.equal('A');
+        expect(queue.tail.value).to.equal('C');
+      });
+
+      it('Should point the next property of the head node to the node that follows it for queues of size one or greater', () => {
+        queue.enqueue('A');
+        queue.enqueue('B');
+        expect(queue.head.next.value).to.eql('B');
+        queue.enqueue('C');
+        expect(queue.head.next.value).to.equal('B');
       });
 
       it('Should increment the queue\'s length by one each time a node is added to the queue', () => {
@@ -117,24 +115,24 @@ describe('Queue', () => {
         expect(queue.dequeue()).to.be.equal(null);
       });
 
-      it('Should reassign the front and back pointers to null if there is only one node in the queue', () => {
+      it('Should reassign the head and tail pointers to null if there is only one node in the queue', () => {
         queue.enqueue('A');
-        expect(queue.front.value).to.equal('A');
-        expect(queue.back.value).to.equal('A');
+        expect(queue.head.value).to.equal('A');
+        expect(queue.tail.value).to.equal('A');
         queue.dequeue();
-        expect(queue.front).to.equal(null);
-        expect(queue.back).to.equal(null);
+        expect(queue.head).to.equal(null);
+        expect(queue.tail).to.equal(null);
       });
 
-      it('Should reassign the front pointer to the node just behind the front node for queues of size two or greater', () => {
+      it('Should reassign the head pointer to the node just behind the head node for queues of size two or greater', () => {
         queue.enqueue('A');
         queue.enqueue('B');
         queue.enqueue('C');
-        expect(queue.front.value).to.equal('A');
+        expect(queue.head.value).to.equal('A');
         queue.dequeue();
-        expect(queue.front.value).to.equal('B');
+        expect(queue.head.value).to.equal('B');
         queue.dequeue();
-        expect(queue.front.value).to.equal('C');
+        expect(queue.head.value).to.equal('C');
       });
 
       it('Should decrement the queue\'s length by one each time a node is removed from the queue', () => {
@@ -153,32 +151,6 @@ describe('Queue', () => {
         queue.enqueue('B');
         expect(queue.dequeue()).to.equal('A');
         expect(queue.dequeue()).to.equal('B');
-      });
-    });
-
-    describe('size', () => {
-      it('Should return a size of 0 for a new queue', () => {
-        expect(queue.size()).to.equal(0);
-      });
-
-      it('Should return a size of 2 after two nodes are added to the queue', () => {
-        queue.enqueue('A');
-        queue.enqueue('B');
-        expect(queue.size()).to.equal(2);
-      });
-
-      it('Should return a size of 1 after two nodes are added and then one is removed', () => {
-        queue.enqueue('A');
-        queue.enqueue('B');
-        queue.dequeue();
-        expect(queue.size()).to.equal(1);
-      });
-
-      it('Should return a size of 0 after more nodes were removed than were added', () => {
-        queue.enqueue('A');
-        queue.dequeue();
-        queue.dequeue();
-        expect(queue.size()).to.equal(0);
       });
     });
 
